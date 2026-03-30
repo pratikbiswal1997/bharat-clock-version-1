f63965607e840438976c9ca7e159fbc3e8824584
pratikbiswal1997 <pratikbiswal1997@gmail.com>
2026-03-30 06:46:02 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ClockName from "./components/ClockName";
+import ClockTime from "./components/ClockTime";
+import Slogan from "./components/Slogan";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ClockName />
+        <Slogan />
+        <ClockTime />
+      </div>
+    </>
+  );
+}
+export default App;
